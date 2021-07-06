df77cf4aff28acd6d70f5b5c6537adc0b78877ab
Andrey Golubev <andpgolubev@gmail.com>
2021-07-06 21:44:21 +0300

    Add stability test to mergesort

--- structs_and_algos/mergesort.cpp
+++ structs_and_algos/mergesort.cpp
@@ -10,11 +10,14 @@
 #include <utility>
 #include <vector>
 
+#include <cassert>
+
 namespace detail {
-template<typename I> void merge(I first1, I last1, I first2, I last2, I d_first) {
+template<typename I, typename Compare>
+void merge(I first1, I last1, I first2, I last2, I d_first, Compare cmp) {
     // [first1, last1) and [first2, last2) are sorted
     while (first1 != last1 && first2 != last2) {
-        if (first1 < first2) {
+        if (cmp(*first1, *first2)) {
             *d_first = *first1;
             ++first1;
         } else {
@@ -33,20 +36,25 @@ template<typename I> void merge(I first1, I last1, I first2, I last2, I d_first)
 }
 }  // namespace detail
 
-template<typename I> void mergesort(I first, I last) {
+template<typename I, typename Compare> void mergesort(I first, I last, Compare cmp) {
     if (first == last || std::next(first) == last) {  // 0 or 1 elements are already sorted
         return;
     }
 
     auto midpoint = first + (std::distance(first, last) / 2);
-    mergesort(first, midpoint);
-    mergesort(midpoint, last);
+    mergesort(first, midpoint, cmp);
+    mergesort(midpoint, last, cmp);
 
     // create temporary buffer
     using T = typename std::iterator_traits<I>::value_type;
     std::vector<T> storage(first, midpoint);
     // merge inplace
-    merge(storage.begin(), storage.end(), midpoint, last, first);
+    merge(storage.begin(), storage.end(), midpoint, last, first, cmp);
+}
+
+template<typename I> void mergesort(I first, I last) {
+    using T = typename std::iterator_traits<I>::value_type;
+    mergesort(first, last, std::less<T>{});
 }
 
 namespace std {
@@ -59,6 +67,26 @@ template<typename T> ostream& operator<<(ostream& os, const std::vector<T>& v) {
     return os;
 }
 }  // namespace std
+
+struct CustomType {
+    bool flag = false;
+    std::string s;
+    int id = -1;
+
+    CustomType(bool f, const std::string& s) : flag(f), s(s) {
+        static int i = 0;
+        id = i++;
+    }
+
+    friend bool operator<(const CustomType& x, const CustomType& y) noexcept { return x.s < y.s; }
+    friend bool operator==(const CustomType& x, const CustomType& y) noexcept {
+        return x.flag == y.flag && x.s == y.s;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const CustomType& x) {
+        os << "(" << (x.flag ? "t" : "f") << ", " << x.s << ", " << x.id << ")";
+        return os;
+    }
+};
 
 int main() {
     TEST_REPEAT_BEGIN("mergesort_small_sizes", 1) {
@@ -114,6 +142,28 @@ int main() {
 
         mergesort(x.begin(), x.end());
         std::sort(y.begin(), y.end());
+
+        return x == y;
+    }
+    TEST_REPEAT_END()
+
+    TEST_REPEAT_BEGIN("mergesort_stability", 1) {
+        std::vector<CustomType> x = {
+            {true, "EAC"},  {false, "EAC"}, {false, "EAC"}, {true, "CDA"},
+            {true, "AE"},   {false, "AA"},  {false, "BB"},  {true, "BAC"},
+            {false, "BAC"}, {true, "BAC"},  {false, "BF"},  {true, "BR"},
+        };
+        std::sort(x.begin(), x.end());
+
+        std::vector<CustomType> y = x;
+
+        struct CompareByFlag {
+            bool operator()(const CustomType& x, const CustomType& y) const noexcept {
+                return x.flag < y.flag;
+            }
+        };
+        mergesort(x.begin(), x.end(), CompareByFlag{});
+        std::stable_sort(y.begin(), y.end(), CompareByFlag{});
 
         return x == y;
     }
